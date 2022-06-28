b0bbdf7e17e68b4b2a76f76c7cebfb1cd99786be
Carlos Cancino-Chacón <carloscancinochacon@gmail.com>
2022-06-28 18:05:09 +0200

    restructure repo (wip)

--- setup.py
+++ setup.py
@@ -3,11 +3,25 @@ import setuptools
 from setuptools import setup
 from Cython.Build import cythonize
 
+# Package meta-data.
+NAME = 'accompanion'
+DESCRIPTION = 'An expressive accompaniment system'
+KEYWORDS = 'music alignment midi audio'
+URL = 'https://github.com/CPJKU/accompanion'
+EMAIL = 'carloscancinochacon@gmail.com'
+AUTHOR = 'Carlos Cancino-Chacón, Silvan Peter, Florian Henkel, Martin Bonev'
+REQUIRES_PYTHON = '>=3.7'
+VERSION = '0.3.0'
+
 setup(
-    name='accompanion',
-    version='0.3.0',
-    description='accompanion',
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
     packages=setuptools.find_packages(),
+    keywords=KEYWORDS,
+    author_email=EMAIL,
+    author=AUTHOR,
+    url=URL,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
@@ -15,7 +29,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering :: MusicInformationRetrieval",
     ],
-    author='Carlos Cancino-Chacón, Silvan Peter, Florian Henkel, Martin Bonev',
     ext_modules=cythonize(
         ['mtchmkr/dtw_loop.pyx'],
         annotate=True),                 # enables generation of the html annotation file
