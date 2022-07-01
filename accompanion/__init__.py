e2ac67109b5ef900de9582c51565f085231a3fe8
melkisedeath <manoskaristineos@gmail.com>
2022-07-01 14:53:45 +0200

    Commented out soundfond imports to make code work.

--- accompanion/__init__.py
+++ accompanion/__init__.py
@@ -6,7 +6,7 @@ import pkg_resources
 from . import accompanist
 from . import misc
 from . import mtchmkr
-from . import sound_fonts
+# from . import sound_fonts
 from . import visualization
 
-SOUND_FONT = pkg_resources.resource_filename("accompanion", "sound_fonts/Acoustic_Piano.sf2")
+# SOUND_FONT = pkg_resources.resource_filename("accompanion", "sound_fonts/Acoustic_Piano.sf2")
