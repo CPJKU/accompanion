b688fe06f16e201f3fb898971772189b665f2bc9
melkisedeath <manoskaristineos@gmail.com>
2023-01-24 21:40:54 +0100

    correction for nan entries.

--- experiments/score_follower/create_solo_match.py
+++ experiments/score_follower/create_solo_match.py
@@ -25,17 +25,18 @@ for file in os.listdir(os.path.join(par_path, "musicxml")):
     note_array = part.note_array()
     a_matched_idx = np.array([d["score_id"] for d in alignment if d["label"] == "match"])
     matched_idx = np.nonzero(np.isin(note_array["id"], a_matched_idx))
+    perf_array["velocity"][np.isnan(perf_array["velocity"])] = 60
     for i in range(5):
         new_perf_array = np.zeros((len(note_array)), np.dtype(
-            [('beat_period', 'f4'), ('timing', 'f4'), ('articulation_log', 'f4'), ('velocity', 'i4')]))
+            [('beat_period', 'f4'), ('timing', 'f4'), ('articulation_log', 'f4'), ('velocity', 'f4')]))
         new_perf_array[matched_idx] = perf_array
         nm_idx = np.where(new_perf_array["beat_period"] == 0)[0]
         new_perf_array[nm_idx]["beat_period"] = perf_array["beat_period"].mean()
-        new_perf_array[nm_idx]["velocity"] = perf_array["velocity"].mean().astype("i4")
+        new_perf_array[nm_idx]["velocity"] = perf_array["velocity"].mean()
         new_perf_array[nm_idx]["articulation_log"] = perf_array["articulation_log"].mean()
         # Add noise to the solo match.
         new_perf_array["beat_period"] = new_perf_array["beat_period"] + np.random.normal(0, 0.1, len(part.note_array()))
-        new_perf_array["velocity"] = new_perf_array["velocity"] + np.random.randint(-10, 10, len(part.note_array()))
+        new_perf_array["velocity"] = new_perf_array["velocity"] + np.random.normal(0, 0.1, len(part.note_array()))
         new_perf_array["articulation_log"] = new_perf_array["articulation_log"] + np.random.normal(0, 0.5, len(part.note_array()))
         new_ppart, new_alignment = pt.musicanalysis.decode_performance(part, new_perf_array, return_alignment=True)
         save_path = os.path.join(par_path, "match_solo", piece_name, piece_name + "_0" + str(i) + ".match")
